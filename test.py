c43f03f31f5cda89f64743eba2f5f3ed8f7671b5
Ondrej Vales <ond.vales@gmail.com>
2018-04-20 19:48:43 +0200

    Time test added

--- test.py
+++ test.py
@@ -3,8 +3,10 @@ import sys
 import random
 import string
 
+time_type = 0
 cnt = 0
 okcnt = 0
+tested_lengths = [x for x in range(1, 20)] + [30, 35, 40, 45, 50, 60, 70, 80, 90, 100]
 
 
 def comp(list1, list2):
@@ -40,28 +42,36 @@ def next(n, position, visits):
             return r
     return p
 
+if len(sys.argv) == 2 and sys.argv[1] == "--times":
+    for i in tested_lengths:
+        total_time = 0.0
+        for _ in range(10):
+            tree = ''.join(random.choices(string.ascii_uppercase + string.digits, k=i))
+            time = subprocess.check_output(["time", "./test.sh", tree], stderr=subprocess.STDOUT).decode('utf-8').split("\n")[1].split(" ")[time_type][:-4]
+            total_time += float(time)
+        print(str(i) + "\t" + str(total_time / 10))
+else:    
+    for i in tested_lengths:
+        tree = ''.join(random.choices(string.ascii_uppercase + string.digits, k=i))
+        out = subprocess.check_output(["./test.sh", tree]).decode('utf-8').split("\n")[:-1][0]
+        pre = preorder(tree)
+        res = comp(pre, out)
 
-for i in range(1, 20):
-    tree = ''.join(random.choices(string.ascii_uppercase + string.digits, k=i))
-    out = subprocess.check_output(["./test.sh", tree]).decode('utf-8').split("\n")[:-1][0]
-    pre = preorder(tree)
-    res = comp(pre, out)
-
-    cnt += 1
-    if res:
-        print(str(i) + " OK")
-        okcnt += 1
+        cnt += 1
+        if res:
+            print(str(i) + " OK")
+            okcnt += 1
+        else:
+            print(str(i) + " FAIL")
+            print("-----Original Input-----")
+            print(tree)
+            print("-----Expected Output----")
+            print(pre)
+            print("-------Your Output------")
+            print(out)
+            
+    print("Score: " + str(okcnt) + "/" + str(cnt))
+    if okcnt == cnt:
+        print("Well done!")
     else:
-        print(str(i) + " FAIL")
-        print("-----Original Input-----")
-        print(tree)
-        print("-----Expected Output----")
-        print(preorder)
-        print("-------Your Output------")
-        print(out)
-        
-print("Score: " + str(okcnt) + "/" + str(cnt))
-if okcnt == cnt:
-    print("Well done!")
-else:
-    print("Some tests are failing!")
+        print("Some tests are failing!")
